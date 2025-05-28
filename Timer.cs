365bbe2c172c8dd41130aae2096cdbed56ec3871
bvelizl <bastianveliz@gmail.com>
2025-05-28 12:32:11 -0400

    Assignment updated. Principal attributes added.

---
+++ Timer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GME1011_A2_BastV
+{
+    internal class Timer
+    {
+        private int _setTimer, _currentTime;
+        private bool _isRunning, _countdown;
+        private Vector2 _locationI, _locationII, _locationIII;
+        private List<Texture2D> _numberI, _numberII, _numberIII;
+    }
+}
